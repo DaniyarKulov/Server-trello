724ce10c57a5e54b5c528830cb26c9f0c4f40f58
DaniyarKulov <kulovdaniyar77@gmail.com>
2023-04-06 04:27:15 +0600

    feat: add coment

--- middlewaree/authMiddle.js
+++ middlewaree/authMiddle.js
@@ -4,7 +4,7 @@ module.exports = function (req, res, next) {
   if (req.method === "OPTIONS") {
     next();
   }
-
+//asdasdasd
   try {
     const token = req.headers.authorization.split(" ")[1];
     if (!token) {
